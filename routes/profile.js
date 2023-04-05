9e21c471e7b4c6b5bedacc72c48bec087a175e79
SajjadReactDeveloper <sajjadeclipse975@gmail.com>
2023-04-05 20:29:19 +0500

    New Apis Edit and delete

--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,7 @@ router.post("/createProfile", profileController.createProfile);
 router.get("/getProfile/:id", profileController.getProfile);
 router.get("/getProfileByUserId/:id", profileController.getProfileByUserId);
 router.get("/", profileController.getAllProfiles);
-router.patch("/updateProfile/:id", profileController.updateProfile);
+router.patch("/updateProfile/:id", profileController.editProfile);
 router.delete("/deleteProfile/:id", profileController.deleteProfile);
 
 module.exports = router;
